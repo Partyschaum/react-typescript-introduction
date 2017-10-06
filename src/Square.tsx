3b51b370e3d679bb9d832eaec7b2ba55d271c95d
Hauke Stange <hauke.stange@jimdo.com>
2017-10-06 11:28:47 +0200

    Add on click handler to Square (for testing)

--- src/Square.tsx
+++ src/Square.tsx
@@ -7,7 +7,7 @@ interface Props {
 export default class Square extends React.Component<Props> {
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={() => alert('click')}>
         {this.props.value}
       </button>
     )
