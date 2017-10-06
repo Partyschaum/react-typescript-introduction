6861224511f0e0ba5e4f563422d8d21a3f78c4a7
Hauke Stange <hauke.stange@jimdo.com>
2017-10-06 13:35:15 +0200

    Toggle current player in Board

--- src/Board.tsx
+++ src/Board.tsx
@@ -5,20 +5,25 @@ interface Props { }
 
 interface State {
   squares: string[]
+  xIsNext: boolean
 }
 
 export default class Board extends React.Component<Props, State> {
   constructor() {
     super()
     this.state = {
-      squares: Array(9).fill('')
+      squares: Array(9).fill(''),
+      xIsNext: true,
     }
   }
 
   handleClick(i: number) {
     const squares = this.state.squares.slice()
-    squares[i] = 'X'
-    this.setState({ squares })
+    squares[i] = this.state.xIsNext ? 'X' : 'O'
+    this.setState({
+      squares,
+      xIsNext: !this.state.xIsNext
+    })
   }
 
   renderSquare(i: number) {
@@ -31,7 +36,7 @@ export default class Board extends React.Component<Props, State> {
   }
 
   render() {
-    const status = 'Next player: X'
+    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`
 
     return (
       <div>
